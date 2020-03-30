8f9fa38b0cb9c425abed929b97b86d748f9d6508
Paolo <p.fiaschi@hotmail.it>
2020-03-30 19:21:55 +0200

    aggiunto click nell'esercizio della parola palindroma

--- parola_palindroma/main.js
+++ parola_palindroma/main.js
@@ -5,36 +5,45 @@
 
 
 var parola = document.getElementById('parola');
-var parolaInserita = prompt("inserire una parola");
-
-
-// funzione per analizza le parola al contrario
-function reverseString(str) {  //parto dall'ultima lettere della parola inserita e con il ciclo scansiono tutte le lettere che la compongono
-    var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
+send.addEventListener("click",
+  function(){
+    var parolaInseritaU = parolaInserita.value;
+    // funzione per analizza le parola al contrario
+    function reverseString(str) {  //parto dall'ultima lettere della parola inserita e con il ciclo scansiono tutte le lettere che la compongono
+        var newString = "";
+        for (var i = str.length - 1; i >= 0; i--) {
+            newString += str[i];
+        }
+        return newString;
     }
-    return newString;
-}
-var reverse = reverseString(parolaInserita);// memorizzo in una var il contenuto della funzione per la parola reverse
-console.log(reverse); // Controllo che si ottenga il reverse della parolaInserita
+    var reverse = reverseString(parolaInseritaU);// memorizzo in una var il contenuto della funzione per la parola reverse
+    console.log(reverse); // Controllo che si ottenga il reverse della parolaInserita
 
 
-//  funzione per vedere se due parole sono o meno palindrome
-function palindroma(par1, par1Rev){
-  var risultato;
-  if (par1 === par1Rev) {
-    risultato = "Parola palindroma"
-  }else {
-    risultato = "Parola NON palindroma"
+    //  funzione per vedere se due parole sono o meno palindrome
+    function palindroma(par1, par1Rev){
+      var risultato;
+      if (par1 === par1Rev) {
+        risultato = " è palindroma"
+
+      }else {
+        risultato = " non è palindroma"
+      }
+      return risultato;
+    }
+
+    var palindroma = palindroma(parolaInseritaU, reverse); // memorizzo in una var il contenuto della funzione per vedere se due parole sono o meno palindrome
+
+    // verifico il risultato finale
+    console.log(palindroma);
+    parola.innerHTML = "La parola " + parolaInseritaU +  palindroma;
+
   }
-  return risultato;
-}
+)
+reset.addEventListener("click",
+  function(){
+    parolaInserita.value = "";
+    parola.innerHTML = "";
 
-var palindroma = palindroma(parolaInserita, reverse); // memorizzo in una var il contenuto della funzione per vedere se due parole sono o meno palindrome
-
-// verifico il risultato finale
-console.log(palindroma);
-parola.innerHTML = "la parola inserita è una " + palindroma;
-
-// document.getElementById('parola').innerHtml = "la parola inserita è una " + palindroma;
+  }
+)
